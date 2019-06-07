b4597c6e87ccfc172b7fcc9768ae64c567253e7f
Madhuri <pittalamadhuri@gmail.com>
2019-06-07 12:04:31 +0530

    README Updated and basic css added to licence key

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const exec=require('child_process').exec;
 const express= require("express");
 var bodyParser = require('body-parser');
 const app = express();
-//var xhr=new XMLHttpRequest;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function (req, res) {
     res.sendFile('D:/MyCodes/licence-cli/input.html');
@@ -14,7 +13,7 @@ app.post('/input', function (req, res) {
             res.send('Error');
         }
         else{
-        res.send(stdout);
+        res.send('<!DOCTYPE html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"><script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script></head><body><div class="tile is-vertical is-1"><div class="tile is-parent"><article class="tile is-child notification is-primary"><div class="content"><p class="title">Licence key</p><div class="content">'+stdout+'</div></div></article></div></div></body></html>');
     }
     //res.send(name+ ' Submitted Successfully!');
     })
